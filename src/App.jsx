dac9a4478442c0ccf8872ec1b9796ed916c7faac
Yash <yg232004@gmail.com>
2024-02-20 00:50:25 +0530

    deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.scss";
 import Contact from "./components/contact/Contact";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
